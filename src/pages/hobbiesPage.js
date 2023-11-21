e1da42000d4307476aa851bfe529dc875e22a2fc
Dylan Windebank <dylanwindebank@my.unt.edu>
2023-11-20 23:19:55 -0600

    iamge color fill on select, also responsiveness stuff

--- src/pages/hobbiesPage.js
+++ src/pages/hobbiesPage.js
@@ -1,22 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Hobbies(){
+    const [ current, setCurrent ] = useState(1);
 
     useEffect(()=>{
         const hobbyContainer = document.getElementById('hobbyContainer')
         hobbyContainer.scrollLeft = (hobbyContainer.scrollWidth - hobbyContainer.clientWidth ) / 2;
+
     }, [])
 
     const scrollLeft = () => {
         const hobbyContainer = document.getElementById('hobbyContainer');
         console.log(hobbyContainer.scrollLeft);
         hobbyContainer.scrollLeft -= hobbyContainer.scrollLeftMax/2;
+        if(current != 0){
+            setCurrent(prevState => prevState-1);
+        }
     }
     const scrollRight = () => {
         const hobbyContainer = document.getElementById('hobbyContainer');
         console.log(hobbyContainer);
         hobbyContainer.scrollLeft += hobbyContainer.scrollLeftMax/2;
+        if(current != 2){
+            setCurrent(prevState => prevState+1);
+        }
     }
 
     return(
@@ -30,14 +38,18 @@ export default function Hobbies(){
             </div>
             <div>
                 <div id="hobbyContainer" >
-                    <div className="leftHobby" />
-                    <div className="hobby climb" />
-                    <div className="hobby ski" />
-                    <div className="hobby game" />
+                    <div className="leftHobby">
+                        <p style={{display: 'block'}}>
+                            sending the white in the corner, this is what i do for something fun and physical.
+                        </p>
+                    </div>
+                    <div className="hobby climb" style={current === 0 ?{webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}}/>
+                    <div className="hobby ski" style={current === 1 ?{webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}} />
+                    <div className="hobby game" style={current === 2 ?{webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}} />
                     <div className="rightHobby">
                         <p style={{display: 'block'}}>
-                                competitive gaming, specifically on the videogame Rocket League, was my main path of personal growth during my highschool and university days. 
-                                I competed on the UNT Varsity Rocket League team as captain for 5 years, lead the team to multiple high stake victories, and even got a national champtionship win.
+                            competitive gaming, specifically on the videogame Rocket League, was my main path of personal growth during my highschool and university days. 
+                            I competed on the UNT Varsity Rocket League team as captain for 5 years, lead the team to multiple high stake victories, and even got a national champtionship win.
                         </p>
                         <br />
                         <p style={{display: 'block'}}>
