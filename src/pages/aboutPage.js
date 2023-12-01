d5d33e9fe5b6f544b167ef9bf3c903c32a086b6f
Dylan Windebank <dylanwindebank@my.unt.edu>
2023-11-30 19:40:53 -0600

    added about me text

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -5,7 +5,9 @@ export default function About(){
         <div className="about flex justify-between">
             <div className="infoCon">
                 <h2>ABOUT ME</h2>
-                <p style={{display: 'block'}}>my name is dylan windebank and i have recently graduated with a b.s. in computer science from the university of north texas. during my time there, i competed on the </p>
+                <p style={{display: 'block'}}>my name is dylan windebank and i have recently graduated with a b.s. in computer science from the university of north texas. 
+                during my time there, i competed on the rocket league team for the university and broadened my programming knowledge. 
+                i am currently a software developer intern at long drive agency but i am looking for a full-time position elsewhere. </p>
             </div>
             <div className="about-image" />
             <div className="about-block1" />
