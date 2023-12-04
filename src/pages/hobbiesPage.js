de180b5871f33202e118cab81959dd1e70d8d8b9
Dylan Windebank <dylanwindebank@my.unt.edu>
2023-12-04 01:14:48 -0600

    changes to hobbies

--- src/pages/hobbiesPage.js
+++ src/pages/hobbiesPage.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function Hobbies(){
     const [ current, setCurrent ] = useState(1);
+    const [ tX, setTX ] = useState(39)
     const [ mobile, setMobile ] = useState(window.innerWidth < 800);
     useEffect(()=>{
         const hobbyContainer = document.getElementById('hobbyContainer')
@@ -11,20 +12,28 @@ export default function Hobbies(){
         function handleResize() {
             
             setMobile(window.innerWidth < 800);
-            
         }
+        
         window.addEventListener("resize", handleResize);
+        
         handleResize();
-        return () => window.removeEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize); 
+        }
     }, [])
 
     const scrollLeft = () => {
         const hobbyContainer = document.getElementById('hobbyContainer');
+        const hobbyLabels = document.getElementById('hobbyLabels');
         if(current===1){
             hobbyContainer.scrollLeft = 0;
+            hobbyLabels.scrollLeft = 0;
+            setTX(68)
         }
         else if(current===2){
             hobbyContainer.scrollLeft = (hobbyContainer.scrollWidth - hobbyContainer.clientWidth)/2;
+            hobbyLabels.scrollLeft = (hobbyLabels.scrollWidth - hobbyLabels.clientWidth)/2;
+            setTX(37)
         }
         if(current !== 0){
             setCurrent(prevState => prevState-1);
@@ -32,11 +41,16 @@ export default function Hobbies(){
     }
     const scrollRight = () => {
         const hobbyContainer = document.getElementById('hobbyContainer');
+        const hobbyLabels = document.getElementById('hobbyLabels');
         if(current===0){
             hobbyContainer.scrollLeft = (hobbyContainer.scrollWidth - hobbyContainer.clientWidth)/2;
+            hobbyLabels.scrollLeft = (hobbyLabels.scrollWidth - hobbyLabels.clientWidth)/2;
+            setTX(37)
         }
         else if(current===1){
             hobbyContainer.scrollLeft = hobbyContainer.scrollWidth - hobbyContainer.clientWidth;
+            hobbyLabels.scrollLeft = hobbyLabels.scrollWidth - hobbyLabels.clientWidth;
+            setTX(0)
         }
         if(current !== 2){
             setCurrent(prevState => prevState+1);
@@ -49,43 +63,54 @@ export default function Hobbies(){
                 <h2>HOBBIES</h2>
                 
             </div>
-            <div>
-                <div id="hobbyContainer" >
-                    <div className="leftHobby">
-                        <p style={{display: 'block'}}>
-                            sending the v0 in the corner, this is what i do for something fun and physical.
-                        </p>
-                    </div>
-                    <div className="hobby climb" style={current=== 0 && !mobile ? {webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}}/>
-                    <div className="hobby ski" style={current === 1 && !mobile ? {webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}} />
-                    <div className="hobby game" style={current === 2 && !mobile ? {webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}} />
-                    <div className="rightHobby">
-                        <p style={{display: 'block'}}>
-                            competitive gaming, specifically on the videogame Rocket League, was my main path of personal growth during my highschool and university days. 
-                            I competed on the UNT Varsity Rocket League team as captain for 5 years, lead the team to multiple high stake victories, and even got a national champtionship win.
-                        </p>
-                        <br />
-                        <p style={{display: 'block'}}>
-                            even if I don't play Rocket League much anymore, gaming will always mean a lot to me and i'll always be playing something.
-                        </p>
-                        <a className='earningsBtn' href="https://liquipedia.net/rocketleague/Dbanq">
-                            <div className="earningsIcon" />
-                        </a>
-                    </div>
-                </div>
-                <div className="pairWrapper">
-                    <div className='btnPair'>
-                        <div style={{height: '95px', paddingTop: '10px'}}>
-                            <div className="socialWrap" onClick={scrollLeft} style={current===0 ? {opacity: '0', cursor: 'auto'}: {}}>
-                                <div id='leftArrow' />
-                            </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <div id="hobbyGallery">
+                    <div className="pairWrapper">
+                        <div className='btnPair'>
+                            
+                                <h3 id="arrow" onClick={scrollLeft} style={current===0 ? {opacity: '0.2', cursor: 'auto'}: {}}>{"<="}</h3>
+                            
+                            
+                                <h3 id="arrow" onClick={scrollRight} style={current===2 ? {opacity: '0.2', cursor: 'auto'}: {}}>{"=>"}</h3>
+                            
+
                         </div>
-                        <div style={{height: '95px', paddingTop: '10px', marginLeft: '12vw'}}>
-                            <div className="socialWrap" style={current===2 ? {opacity: '0', cursor: 'auto'}: {}} onClick={scrollRight} >
-                                <div id='rightArrow' />
+                        <div style={{position: 'relative'}}>
+                            <div id ="hobbyGradient" />
+                            <div id="hobbyLabels" style={{transform: `translateX(${tX}%)`}}>
+                                <h2 className="hobbyLabel">climbing</h2>
+                                <h2 className="hobbyLabel">skiing</h2>
+                                <h2 className="hobbyLabel">gaming</h2>
                             </div>
                         </div>
                     </div>
+                    <div id="hobbyContainer" >
+                        {/*}
+                        <div className="leftHobby">
+                            <p style={{display: 'block'}}>
+                                sending the v0 in the corner, this is what i do for something fun and physical.
+                            </p>
+    </div>*/}
+                        <div className="hobby climb" style={current=== 0 && !mobile ? {webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}}/>
+                        <div className="hobby ski" style={current === 1 && !mobile ? {webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}} />
+                        <div className="hobby game" style={current === 2 && !mobile ? {webkitFilter: 'grayscale(0%)', mozFilter: 'grayscale(0%)', filter: 'grayscale(0%)'}: {}} />
+                        {/*}
+                        <div className="rightHobby">
+                            <p style={{display: 'block'}}>
+                                competitive gaming, specifically on the videogame Rocket League, was my main path of personal growth during my highschool and university days. 
+                                I competed on the UNT Varsity Rocket League team as captain for 5 years, lead the team to multiple high stake victories, and even got a national champtionship win.
+                            </p>
+                            <br />
+                            <p style={{display: 'block'}}>
+                                even if I don't play Rocket League much anymore, gaming will always mean a lot to me and i'll always be playing something.
+                            </p>
+                            <a className='earningsBtn' href="https://liquipedia.net/rocketleague/Dbanq">
+                                <div className="earningsIcon" />
+                            </a>
+                        </div>
+    */}
+                    </div>
+                    
                 </div>
             </div>
         </div>
